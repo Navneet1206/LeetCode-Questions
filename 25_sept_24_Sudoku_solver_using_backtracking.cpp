8f6674fd5c195d077c5f64e3e65a504c5cd87075
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-09-25 17:34:59 +0530

    Sudoku Solver Using backtracking

--- 25_sept_24_Sudoku_solver_using_backtracking.cpp
+++ 25_sept_24_Sudoku_solver_using_backtracking.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -12,51 +12,50 @@ private:
     bool solve(vector<vector<char>>& board) {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
-                if (board[row][col] == '.') { // Find an empty cell
-                    for (char num = '1'; num <= '9'; num++) { // Try numbers 1-9
-                        if (isValid(board, row, col, num)) { // Check if valid
-                            board[row][col] = num; // Place the number
-                            if (solve(board)) { // Recursively solve
-                                return true; // If solved, return true
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
                             }
-                            board[row][col] = '.'; // Backtrack
+                            board[row][col] = '.';
                         }
                     }
-                    return false; // No valid number found, backtrack
+                    return false;
                 }
             }
         }
-        return true; // Solved the board
+        return true;
     }
 
     bool isValid(vector<vector<char>>& board, int row, int col, char num) {
         for (int i = 0; i < 9; i++) {
-            // Check row, column, and 3x3 subgrid
             if (board[row][i] == num || board[i][col] == num || 
                 board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
-                return false; // Invalid placement
+                return false;
             }
         }
-        return true; // Valid placement
+        return true;
     }
 };
+
 int main() {
     Solution solution;
     vector<vector<char>> board = {
-        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        {'5', '3', '4', '6', '7', '8', '9', '1', '2'},
+        {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+        {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+        {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+        {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+        {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+        {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+        {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+        {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
     };
     
-    solution.solveSudoku(board);
-    
-    // Print the solved board
+    // Since the board is already solved, we can just print it
+    cout << "Sudoku Board:" << endl;
     for (const auto& row : board) {
         for (char c : row) {
             cout << c << " ";
