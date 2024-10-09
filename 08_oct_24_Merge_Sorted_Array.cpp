bff2fa8e77bc3e8b34cb654cf54425f3a234532f
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-10-09 15:59:13 +0530

    Merge is updated

--- 08_oct_24_Merge_Sorted_Array.cpp
+++ 08_oct_24_Merge_Sorted_Array.cpp
@@ -1,46 +1,66 @@
 #include <iostream>
 #include <vector>
-#include<algorithm>
 using namespace std;
 
-int main()
-{
-    int n;
-    cout<<"Enter the size of the array : ";
-    cin>>n;
-    vector<int> v(n);
-    vector<int> copy(n);
-    int k = 0;
-    
-    for(int i=0;i<n;i++)
-    {
-        cout<<"Enter the "<<i+1<<" element : ";
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;  
+        int j = n - 1;  
+        int k = m + n - 1;  
         
-        cin>>v[i];
-    }
-
-    sort(v.begin(),v.end());
-
-   
-    for(int i=0;i<n;i++)
-    {
-        if(v[i]!=0)
-        {
-            copy[k++]=v[i];
+        // Merge nums1 and nums2 from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
 
+        // If there are remaining elements in nums2, add them
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
-     cout<<"Output is with zero  : \n";
-    for(int i=0;i<n;i++)
-    {
-        cout<<v[i]<<" ";
+};
+
+int main() {
+    Solution solution;
+
+    // User input for nums1
+    int m, n;
+    cout << "Enter the number of initialized elements in nums1: ";
+    cin >> m;
+    cout << "Enter the number of initialized elements in nums2: ";
+    cin >> n;
+
+    vector<int> nums1(m + n); // Create nums1 with enough space
+    cout << "Enter the elements of nums1 (sorted): ";
+    for (int i = 0; i < m; ++i) {
+        cin >> nums1[i];
     }
-    cout<<endl;
-    cout<<"Output is without zero  : \n";
-    for(int i=0;i<k;i++)
-    {
-        cout<<copy[i]<<" ";
+
+    // Fill the remaining space in nums1 with zeros
+    for (int i = m; i < m + n; ++i) {
+        nums1[i] = 0; // Assuming the remaining space is filled with zeros
     }
-    
+
+    vector<int> nums2(n);
+    cout << "Enter the elements of nums2 (sorted): ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums2[i];
+    }
+
+    // Call the merge function
+    solution.merge(nums1, m, nums2, n);
+
+    // Output the merged array
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
