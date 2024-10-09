0d886d46c5cb4b2926a36b7f9ebe35520c004753
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-10-09 16:27:21 +0530

    Questions are added

--- 08_oct_24_Merge_Sorted_Array.cpp
+++ 08_oct_24_Merge_Sorted_Array.cpp
@@ -2,30 +2,36 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;  
-        int j = n - 1;  
-        int k = m + n - 1;  
-        
-        // Merge nums1 and nums2 from the end
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
-        }
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int i = m - 1;
+        int j = n - 1;
 
-        // If there are remaining elements in nums2, add them
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        for (int k = m + n - 1; k >= 0; k--)
+        {
+            if (j < 0)
+            {
+                break;
+            }
+            if (i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
         }
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
 
     // User input for nums1
@@ -37,18 +43,21 @@ int main() {
 
     vector<int> nums1(m + n); // Create nums1 with enough space
     cout << "Enter the elements of nums1 (sorted): ";
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+    {
         cin >> nums1[i];
     }
 
     // Fill the remaining space in nums1 with zeros
-    for (int i = m; i < m + n; ++i) {
+    for (int i = m; i < m + n; ++i)
+    {
         nums1[i] = 0; // Assuming the remaining space is filled with zeros
     }
 
     vector<int> nums2(n);
     cout << "Enter the elements of nums2 (sorted): ";
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
         cin >> nums2[i];
     }
 
@@ -57,7 +66,8 @@ int main() {
 
     // Output the merged array
     cout << "Merged array: ";
-    for (int num : nums1) {
+    for (int num : nums1)
+    {
         cout << num << " ";
     }
     cout << endl;
