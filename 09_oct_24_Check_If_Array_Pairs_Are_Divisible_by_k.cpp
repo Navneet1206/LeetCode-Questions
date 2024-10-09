2fea6c40db2f8ae0f767a4a0c7ca2cde887c2d54
Navneet Vishwakarma <rajvl132011@gmail.com>
2024-10-09 17:29:31 +0530

    Updated

--- 09_oct_24_Check_If_Array_Pairs_Are_Divisible_by_k.cpp
+++ 09_oct_24_Check_If_Array_Pairs_Are_Divisible_by_k.cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, count = 0;
+    int sum = 0;
+    int chem;
+    cout << "Enter the number of the elements : ";
+    cin >> n;
+    if (n % 2 != 0)
+    {
+        cout << "-1";
+        exit(0);
+    }
+    vector<int> skill(n);
+    cout << "Enter the elements : \n";
+    for (int i = 0; i < skill.size(); i++)
+    {
+        cin >> skill[i];
+    }
+    cout << "Enter the K value : ";
+    cin >> chem;
+    int last = skill.size() - 1, first = 0;
+
+    while (first < last)
+    {
+        if (((skill[first] + skill[last]) % chem) == 0)
+        {
+            count++;
+        }
+        else
+        {
+            cout << "False";
+            exit(0);
+        }
+        --last;
+        ++first;
+    }
+
+    if (count == (n / 2))
+        cout << "\n"
+             << "True";
+
+    return 0;
+}
+
 /*
 Given an array of integers arr of even length n and an integer k.
 
@@ -5,7 +52,7 @@ We want to divide the array into exactly n / 2 pairs such that the sum of each p
 
 Return true If you can find a way to do that or false otherwise.
 
- 
+
 
 Example 1:
 
@@ -22,7 +69,7 @@ Example 3:
 Input: arr = [1,2,3,4,5,6], k = 10
 Output: false
 Explanation: You can try all possible pairs to see that there is no way to divide arr into 3 pairs each with sum divisible by 10.
- 
+
 
 Constraints:
 
